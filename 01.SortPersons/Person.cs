a8a417d891eae2687bf85e48ed1fc9b5838f27cd
morskijk <idontneeddisshit@gmail.com>
2018-03-10 04:00:44 +0200

    push first task - enc

---
+++ 01.SortPersons/Person.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+	const decimal MIN_SALARY = 460;
+	const int MIN_LENGTH = 3;
+
+	private string firstName;
+
+	public string FirstName
+	{
+		get { return firstName; }
+		private set
+		{
+			if (value?.Length < MIN_LENGTH)
+			{
+				throw new ArgumentException($"First name cannot contain fewer than {MIN_LENGTH} symbols");
+			}
+			firstName = value;
+		}
+	}
+
+	private string lastName;
+
+	public string LastName
+	{
+		get { return lastName; }
+		private set
+		{
+			if (value?.Length < MIN_LENGTH)
+			{
+				throw new ArgumentException($"Last name cannot contain fewer than {MIN_LENGTH} symbols");
+			}
+			lastName = value;
+		}
+	}
+
+	private int age;
+
+	public int Age
+	{
+		get { return age; }
+		private set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Age cannot be zero or negative integer!");
+			}
+			age = value;
+		}
+	}
+
+	private decimal salary;
+
+	public decimal Salary
+	{
+		get { return salary; }
+		private set
+		{
+			if (value < MIN_SALARY)
+			{
+				throw new ArgumentException($"Salary cannnot be less than {MIN_SALARY} leva!");
+			}
+			salary = value;
+		}
+	}
+
+
+	public Person(string firstName, string lastName, int age, decimal salary)
+	{
+		this.FirstName = firstName;
+		this.LastName = lastName;
+		this.Age = age;
+		this.Salary = salary;
+	}
+
+	public void IncreaseSalary(decimal percentage)
+	{
+		if (this.age > 30)
+		{
+			salary += salary * percentage / 100;
+		}
+		else
+		{
+			salary += salary * percentage / 200;
+		}
+
+	}
+
+	public override string ToString()
+	{
+		return $"{firstName} {lastName} receives {salary:f2} leva.";
+	}
+}
+
---
+++ 01.SortPersons/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var n = int.Parse(Console.ReadLine());
+		var people = new List<Person>();
+		for (int i = 0; i < n; i++)
+		{
+			var personData = Console.ReadLine().Split();
+			try
+			{
+				Person person = new Person(personData[0], personData[1], int.Parse(personData[2]), decimal.Parse(personData[3]));
+				people.Add(person);
+			}
+			catch (ArgumentException ArgEx)
+			{
+				Console.WriteLine(ArgEx.Message);
+			}
+
+		}
+		var bonus = decimal.Parse(Console.ReadLine());
+		foreach (var person in people)
+		{
+			person.IncreaseSalary(bonus);
+			Console.WriteLine(person.ToString());
+		}
+	}
+}
