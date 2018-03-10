2a04a4381987491a44cb5896abac900db6152e72
morskijk <idontneeddisshit@gmail.com>
2018-03-10 04:02:36 +0200

    push enc tasks

---
+++ 02.FirstAndReserveTeam/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var n = int.Parse(Console.ReadLine());
+		var players = new Team("nameless");
+		for (int i = 0; i < n; i++)
+		{
+			var personArgs = Console.ReadLine().Split();
+			Person person = new Person(personArgs[0], personArgs[1], int.Parse(personArgs[2]), decimal.Parse(personArgs[3]));
+			players.AddPlayer(person);
+		}
+		Console.WriteLine($"First team has {players.FirstTeam.Count} players.");
+		Console.WriteLine($"Reserve team has {players.ReserveTeam.Count} players.");
+	}
+}
---
+++ 02.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Team
+{
+	private string name;
+	private List<Person> firstTeam;
+	private List<Person> reserveTeam;
+
+	public IReadOnlyCollection<Person> FirstTeam
+	{
+		get { return firstTeam; }
+	}
+
+	public IReadOnlyCollection<Person> ReserveTeam
+	{
+		get { return reserveTeam; }
+	}
+
+	public Team(string teamName)
+	{
+		this.firstTeam = new List<Person>();
+		this.reserveTeam = new List<Person>();
+		this.name = teamName;
+	}
+
+	public void AddPlayer(Person player)
+	{
+		if (player.Age < 40)
+		{
+			firstTeam.Add(player);
+		}
+		else
+		{
+			reserveTeam.Add(player);
+		}
+	}
+
+}
+
